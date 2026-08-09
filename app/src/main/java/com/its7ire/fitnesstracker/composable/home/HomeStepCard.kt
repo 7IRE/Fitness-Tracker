a05fa1b7f76ad51f32692c62da470adc142f3cee
Rewant <rairewant26@gmail.com>
2026-08-09 19:49:22 +0530

    Step Goal settings

--- app/src/main/java/com/its7ire/fitnesstracker/composable/home/HomeStepCard.kt
+++ app/src/main/java/com/its7ire/fitnesstracker/composable/home/HomeStepCard.kt
@@ -2,6 +2,7 @@ package com.its7ire.fitnesstracker.composable.home
 
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -49,6 +50,7 @@ fun HomeStepCard(
     Box(
         modifier = modifier
             .fillMaxWidth()
+            .clickable{}
             .aspectRatio(1.05f)
             .clip(RoundedCornerShape(24.dp))
             .background(
